49b912cc604f92fe3f621c706c2699778389dc31
Hayssam Saleh <hayssam@saleh.fr>
2026-06-14 16:38:15 +0200

    cls: rewriter masks inside CAST

--- src/test/scala/ai/starlake/quack/edge/cls/JsqltranspilerRewriterSpec.scala
+++ src/test/scala/ai/starlake/quack/edge/cls/JsqltranspilerRewriterSpec.scala
@@ -55,3 +55,14 @@ class JsqltranspilerRewriterSpec extends AnyFlatSpec with Matchers:
       case RewriteOutcome.Rewritten(sql) => sql should include ("'***'")
       case other                          => fail(s"expected Rewritten, got $other")
   }
+
+  it should "mask c_email inside CAST" in {
+    val out = rw.rewrite(
+      "SELECT CAST(c_email AS VARCHAR) FROM customer",
+      schema, List(maskEmail),
+      Some("acme_tpch"), Some("tpch1")
+    )
+    out match
+      case RewriteOutcome.Rewritten(sql) => sql should include ("'***'")
+      case other                          => fail(s"expected Rewritten, got $other")
+  }
