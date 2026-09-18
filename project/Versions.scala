1c3506bd92979bfcb12210ca260041fc90cff2cb
Hayssam Saleh <hayssam@saleh.fr>
2026-09-18 13:19:49 +0200

    Update jsqlparser

--- project/Versions.scala
+++ project/Versions.scala
@@ -13,7 +13,7 @@ object Versions {
   val nimbusJoseJwt  = "9.47"
   val scalaTest      = "3.2.17"
   // jsqlParser must match the version jsqltranspiler's pom pins (its manticore line)
-  val jsqlParser     = "5.4.2"
+  val jsqlParser     = "5.4.15"
   val jsqltranspiler = "1.12"
   val circeYaml      = "0.16.1"
   val cats           = "2.13.0"
